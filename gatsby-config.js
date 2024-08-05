bdbef459a093bd96be3c7adb0121378b269df3fe
Bryan Enid <bryanenid97@gmail.com>
2024-08-04 23:05:17 -0400

    Added necessary contentful settings

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,26 @@
+require("dotenv").config({ path: `.env.${process.env.NODE_ENV}` });
+
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
   siteMetadata: {
     title: `Mileidy's Maids`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [{
-    resolve: 'gatsby-source-contentful',
-    options: {
-      "accessToken": "iizPBysVkDYIPIhVdbGn4o7Fo6LNfImvnUjJ3PQPe6w",
-      "spaceId": "1zl7tyk46ayt"
-    }
-  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-postcss", "gatsby-plugin-sitemap"]
+  plugins: [
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        host: `preview.contentful.com`,
+      },
+    },
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-postcss",
+    "gatsby-plugin-sitemap",
+  ],
 };
